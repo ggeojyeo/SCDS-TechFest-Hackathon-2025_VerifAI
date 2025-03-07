b9fa3a4fe8e1bd751bdaf93b1c8ae3cdaac82806
Adolphus Chua <github.adolphus@gmail.com>
2025-03-07 22:35:12 +0800

    implemented a image upload function and images uploaded get stored in a directory

---
+++ tvbd/src/app/textVerification/page.tsx
@@ -0,0 +1,10 @@
+import ImageUpload from "../../components/ImageUpload";
+
+export default function UploadPage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-center mt-4">Upload Images</h2>
+      <ImageUpload />
+    </div>
+  );
+}
