5e156bce074139ffddc03deb00e538ad5c61f7e0
Adolphus Chua <github.adolphus@gmail.com>
2025-03-09 16:37:09 +0800

    intial commit with sample setup

--- tvbd/tesseract-ocr-test.js
+++
@@ -1,7 +0,0 @@
-const Tesseract = require("tesseract.js");
-
-Tesseract.recognize("./straitsTimes_Extract.png", "eng")
-    .then(({ data: { text } }) => {
-        console.log("Extracted text:", text);
-    })
-    .catch(err => console.error("Tesseract Error:", err));
